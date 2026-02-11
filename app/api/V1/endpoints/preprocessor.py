a2d21fd86c070b41cbb44d9fed120dd70dbdc598
sss-2205 <shahid.sayyad2205@gmail.com>
2026-02-11 15:07:09 +0530

    branch update

--- app/api/V1/endpoints/preprocessor.py
+++ app/api/V1/endpoints/preprocessor.py
@@ -17,5 +17,3 @@ async def preprocess(data: Article):
 
     return response.json()
 
-
-# change according to preprocessor
--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ def create_app() -> FastAPI:
             "http://127.0.0.1:3000",
             "https://your-frontend-domain.vercel.app",
             "https://cors-test-vercel-news-scrape.vercel.app"
+            "https://pipeline-ta80.onrender.com"
         ],
         allow_credentials=True,
         allow_methods=["*"],
